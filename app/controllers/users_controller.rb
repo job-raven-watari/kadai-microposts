690146536ca3c2fc66a1f2b34f654262ca94bde5
job.raven.watari <job_raven_watari@yahoo.co.jp>
2019-03-26 04:59:56 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
   
   # Strong Parameter(lesson9-8.7)
