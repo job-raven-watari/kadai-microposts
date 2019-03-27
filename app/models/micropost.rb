86e899b8b6cf9de2d75ec4a11061e45368411812
job.raven.watari <job_raven_watari@yahoo.co.jp>
2019-03-27 08:15:32 +0000

    like, unlike

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :likes
+  has_many :users, through: :likes, dependent: :destroy
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
